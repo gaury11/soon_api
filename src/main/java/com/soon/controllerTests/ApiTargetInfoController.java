f55b7fcb26277ddf001ffc62169a7cbc9c471f75
soon <>
2019-02-21 09:26:04 +0900

    저장 기능 테스트 (아직 테이블이 없어서 저장은 안됨) 1. Map<String, Object> params 로 파라미터 받음 2. ApiTargetInfo 객체에 생성하여 저장 진행

--- src/main/java/com/soon/controllerTests/ApiTargetInfoController.java
+++ src/main/java/com/soon/controllerTests/ApiTargetInfoController.java
@@ -1,23 +1,29 @@
-package com.soon.controller;
+package com.soon.controllerTests;
 
 import com.soon.service.ApiTargetInfoService;
 import com.soon.domain.ApiTargetInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @CrossOrigin
 @RequestMapping("/apiTarget")
 public class ApiTargetInfoController {
 
+
+
     /*
         왜 여기서는 의존성 주입을 하지???
         차이가 뭐여
      */
+
     @Autowired
     ApiTargetInfoService apiTargetInfoService;
 
@@ -36,10 +42,19 @@ public class ApiTargetInfoController {
         return entity;
     }
 
-    @PostMapping
-    public String postApiTargetInfo(@RequestBody ApiTargetInfo apiTargetInfo){
+    @PostMapping("/save")
+    public String postApiTargetInfo(@RequestBody Map<String, Object> params){
+
+        log.info("params : {}", params.toString());
+
+        ApiTargetInfo apiTargetInfo = ApiTargetInfo.builder()
+                .targetName(params.get("targetName").toString())
+                .targetUrl(params.get("targetUrl").toString())
+                .build();
 
         apiTargetInfo.setRegDateNow();
+
+        log.info("apiTargetInfo - name : {} / url :{}", apiTargetInfo.getTargetName(), apiTargetInfo.getTargetUrl());
 
         apiTargetInfoService.saveApiTargetInfo(apiTargetInfo);
 
